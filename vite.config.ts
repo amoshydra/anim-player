1bb6cec8e92058cae91a7a17429bc71cd653332a
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-06-23 06:52:24 +0800

    ci: setup ci

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import react from '@vitejs/plugin-react';
 import wyw from '@wyw-in-js/vite';
 import { defineConfig } from 'vite';
 
+const BASE_URL = process.env.BASE_URL || "";
+
 export default defineConfig({
+  base: `${BASE_URL}/`,
   plugins: [
     react(),
     wyw(),
