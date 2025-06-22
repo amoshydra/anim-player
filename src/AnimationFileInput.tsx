9e5b69e6c77e0dc9aef9c9b3040c7a136686dfa1
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-06-23 06:46:55 +0800

    feat: allow loading of asset via file query param

--- src/AnimationFileInput.tsx
+++ src/AnimationFileInput.tsx
@@ -1,5 +1,5 @@
 import { css } from '@linaria/core';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export interface AnimationFileInputProps {
   onFileChange: (url: string) => void;
@@ -9,7 +9,6 @@ export const AnimationFileInput: React.FC<AnimationFileInputProps> = ({ onFileCh
   return (
     <AnimationFileButton
       onFileChange={(content) => {
-        console.log(content);
         onFileChange(content[0]);
       }}
     />
@@ -22,6 +21,75 @@ interface AnimationFileButtonProps {
 }
 const AnimationFileButton: React.FC<AnimationFileButtonProps> = ({ onFileChange }) => {
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const fileUrl = params.get("file");
+    if (fileUrl) {
+      setLoading(true);
+      fetch(fileUrl).then((r) => {
+        if (r.headers.get("content-type") === "application/json") {
+          return r.blob().then(b => {
+            const blobUrl = URL.createObjectURL(b);
+            onFileChange([blobUrl]);
+          })
+        }
+        return getUrl(r);
+      }).finally(() => {
+        setLoading(false);
+      })
+    }
+  }, []);
+
+
+  const getUrl = async (file?: File | Response) => {
+    setLoading(true);
+    try {
+      if (!file) return;
+
+      if (file instanceof File && file.name.endsWith(".json")) {
+        // Handle JSON files
+        const blobUrl = URL.createObjectURL(file);
+        onFileChange([blobUrl]);
+        return;
+      }
+
+      // Handle ZIP files
+      try {
+        const arrayBuffer = await file.arrayBuffer();
+        const uint8Array = new Uint8Array(arrayBuffer);
+        const { default: JSZip } = await import('jszip/dist/jszip.min.js');
+        const unzippedFiles = await JSZip.loadAsync(uint8Array);
+        const jsonFileNames = Object.keys(unzippedFiles.files)
+          .filter(name => {
+            if (!name.endsWith('.json')) return false
+            if (name.startsWith('animations/') || name.startsWith('a/')) return true;
+            return false;
+          });
+        if (jsonFileNames.length) {
+          const blobs: string[] = []
+          for (const fileName of jsonFileNames) {
+            const jsonBlob = await unzippedFiles.file(fileName)!.async('blob');
+            const blobUrl = URL.createObjectURL(jsonBlob);
+            blobs.push(blobUrl);
+          }
+          onFileChange(blobs);
+        } else {
+          console.warn("No JSON file found in ZIP");
+          alert("No Lottie animation JSON file found in the ZIP archive.");
+          return;
+        }
+      } catch (error) {
+        console.error("Error processing ZIP file:", error);
+        alert("Failed to process ZIP file. Please try again.");
+        return;
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+
   return (
     <label className={cssButton} tabIndex={0}>
       {loading ? "Processing..." : "Load your own animation"}
@@ -29,57 +97,8 @@ const AnimationFileButton: React.FC<AnimationFileButtonProps> = ({ onFileChange 
         type="file"
         style={{ display: "none" }}
         accept=".json,.zip,.lottie"
-        onChange={async (e) => {
-          setLoading(true);
-          try {
-            const file = e.target.files?.[0];
-            if (!file) return;
-
-            if (file.name.endsWith(".json")) {
-              // Handle JSON files
-              const blobUrl = URL.createObjectURL(file);
-              onFileChange([blobUrl]);
-              return;
-            }
-            
-            if (!(file.name.endsWith(".zip") || file.name.endsWith(".lottie"))) {
-              console.warn("Unsupported file format");
-              alert("Unsupported file format. Please upload a .json or .zip file.");
-              return;
-            }
-            // Handle ZIP files
-            try {
-              const arrayBuffer = await file.arrayBuffer();
-              const uint8Array = new Uint8Array(arrayBuffer);
-              const { default: JSZip } = await import('jszip/dist/jszip.min.js');
-              const unzippedFiles = await JSZip.loadAsync(uint8Array);
-              const jsonFileNames = Object.keys(unzippedFiles.files)
-                .filter(name => {
-                  if (!name.endsWith('.json')) return false
-                  if (name.startsWith('animations/') || name.startsWith('a/')) return true;
-                  return false;
-                });
-              if (jsonFileNames.length) {
-                const blobs: string[] = []
-                for (const fileName of jsonFileNames) {
-                  const jsonBlob = await unzippedFiles.file(fileName)!.async('blob');
-                  const blobUrl = URL.createObjectURL(jsonBlob);
-                  blobs.push(blobUrl);
-                }
-                onFileChange(blobs);
-              } else {
-                console.warn("No JSON file found in ZIP");
-                alert("No Lottie animation JSON file found in the ZIP archive.");
-                return;
-              }
-            } catch (error) {
-              console.error("Error processing ZIP file:", error);
-              alert("Failed to process ZIP file. Please try again.");
-              return;
-            }
-          } finally {
-            setLoading(false);
-          }
+        onChange={(e) => {
+          getUrl(e.currentTarget.files?.[0])
         }}
       />
     </label>
