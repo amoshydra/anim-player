86bc41408b858cbba647c7b43acb70e62ea353f6
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-07-07 00:56:26 +0800

    refactor: reuse as many lottie original logic as possible

--- src/classes/EnhancedLottiePlayer.ts
+++ src/classes/EnhancedLottiePlayer.ts
@@ -12,89 +12,29 @@ export class EnhancedLottiePlayer<T extends RendererType = 'svg'> {
   instance: ControlledAnimation;
 
   segments: AnimationSegment[] = [];
-  isPlaying = false;
-  prevElapsed = 0;
-  isRunning = true
+  realTotalFrames: number = 0;
+  completed: boolean = false;
 
   constructor(config: AnimationConfigWithPath<T> | AnimationConfigWithData<T>) {
     this.config = config;
 
-    const _animation = lottie.loadAnimation({
-      ...config,
-      autoplay: false, // we will control this ourselves
-    });
+    const _animation = lottie.loadAnimation(config);
 
     const m = this;
-;   m.isPlaying = config.autoplay || false;
-
-    // Setup animation runner
-    const runAnimation = (elapsed: number) => {
-      if (!m.isRunning) return;
-
-      if (!animation.isPaused) {
-        const marker = animation.segments[0]
-
-        const START = marker[0];
-        const END = marker[1];
-
-        const delta = elapsed - m.prevElapsed;
-        const advance = delta / 1000 * animation.frameRate;
-        let nextFrame = (animation.currentFrame || START) + advance * animation.playDirection;
-        nextFrame = Math.max(START, Math.min(nextFrame, END + 1));
-
-        if (nextFrame > END) {
-          if (animation.segments.length > 1) {
-            animation.segments.shift();
-            nextFrame = animation.segments[0][0];
-          } else if (animation.loop) {
-            nextFrame = START;
-          } else {
-            animation.pause();
-          }
-        } else if (nextFrame < START) {
-          if (animation.loop) {
-            nextFrame = END;
-          } else {
-            animation.pause();
-          }
-        }
-        // @ts-expect-error - this is a private method
-        animation.setCurrentRawFrameValue(nextFrame);
-      }
-
-      m.prevElapsed = elapsed;
-      requestAnimationFrame(runAnimation);
-    };
-
     const animation = new Proxy(_animation, {
       get(target, property, receiver) {
-        if (property === 'isPaused') {
-          return !m.isPlaying;
+        if (property === 'play') {
+          m.completed = false;
+          return Reflect.get(target, property, receiver);
         }
         if (property === 'segments') {
           if (m.segments.length === 0) {
-            return [[0, animation.totalFrames]]
+            return [[0, m.realTotalFrames]]
           }
           return m.segments;
         }
-        if (property === 'autoplay') {
-          return config.autoplay ?? false;
-        }
 
-        // methods
-        if (property === 'play') {
-          return () => {
-            m.isPlaying = true;
-            requestAnimationFrame(runAnimation);
-          }
-        }
-        if (property === 'pause') {
-          return () => {
-            m.isPlaying = false;
-          }
-        }
         if (property === 'enqueueSegments') {
-          m.isPlaying = true;
           return (incomingSegments: AnimationSegment[], dropUnPlayedSegment = false): void => {
             const existingSegments = m.segments.slice(0, dropUnPlayedSegment ? 1 : m.segments.length);
             m.segments = [...existingSegments, ...incomingSegments];
@@ -105,10 +45,63 @@ export class EnhancedLottiePlayer<T extends RendererType = 'svg'> {
     }) as ControlledAnimation;
     this.instance = animation;
 
+    _animation.addEventListener("config_ready", () => {
+      this.realTotalFrames = animation.totalFrames;
+    });
+    _animation.addEventListener("enterFrame", ({ currentTime }) => {
+      this._renderFrame({ currentTime })
+    });
+    // handle when playback reached the last marker, we will resume playback if there are more segments to play
+    _animation.addEventListener("complete", () => {
+      if (this.segments.length > 0) {
+        // @TODO: for actual usage, we may need to suprress this if there are indeed more segment to play
+        const [start] = this.segments[0];
+        if (!this.completed) {
+          animation.goToAndPlay(start);
+        }
+        if (this.segments.length > 1) {
+          animation.goToAndPlay(start);
+        }
+      }
+    })
+  }
 
-    if (config.autoplay) {
-      requestAnimationFrame(runAnimation);
+  _renderFrame({ currentTime }: { currentTime: number }) {
+    const segments = this.instance.segments;
+    const marker = segments[0]
+
+    const [start, _end] = marker;
+    const end = _end - 1;
+
+    let nextFrame = currentTime;
+    nextFrame = Math.max(start, Math.min(nextFrame, end));
+
+    if (nextFrame >= end) {
+      if (segments.length > 1) {
+        // drop current segment
+        this.segments.shift();
+        // play next segment's start
+        const next = segments[0];
+        nextFrame = next[0];
+      } else {
+        nextFrame = start;
+        if (!this.instance.loop) {
+          this.completed = true;
+        }
+      }
+    } else if (nextFrame < start) {
+      nextFrame = end;
+      if (!this.instance.loop) {
+        this.completed = true;
+      }
     }
+
+    if (this.completed) {
+      if (!this.instance.isPaused) {
+        this.instance.stop();
+      }
+    }
+    this.instance.currentRawFrame = nextFrame;
   }
 
   destroy(name?: string) {
